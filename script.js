07f45f074b3c6ca69d83b0929b19b10fa077711b
christiannaira <christianmnaira@gmail.com>
2023-05-17 14:51:34 +0630

    my first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let numbers = document.querySelectorAll(".number");
+let output = document.querySelector(".output");
+let elements = document.querySelectorAll(".element");
+let num1 = '';
+let result = '';
+
+function display() {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", function () {
+            if (!elements[i].className.includes('operator')) {
+                num1 += elements[i].textContent.trim();
+                output.innerHTML = num1;
+            } else {
+                output.innerHTML = elements[i].innerHTML;
+                num1 = '';
+            }
+
+            result += elements[i].textContent.trim();
+        })
+
+    }
+}
+
+document.querySelector('.equal').addEventListener("click", function () {
+    output.innerHTML = eval(result);
+})
+
+function reset() {
+    output.innerHTML = '0';
+    num1 = '';
+    result = '';
+}
+
+
+
+display();
+
